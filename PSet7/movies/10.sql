143f1c5a73ee50568aa7f8ea7bb3375795106229
cuddlywhiskers <54569808+cuddlywhiskers@users.noreply.github.com>
2020-05-01 18:23:01 +0800

    Create 10.sql

---
+++ PSet7/movies/10.sql
@@ -0,0 +1,12 @@
+-- write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0
+
+SELECT
+    DISTINCT(name)
+FROM people
+JOIN directors
+    ON people.id = directors.person_id
+JOIN movies
+    ON movies.id = directors.movie_id
+JOIN ratings
+    ON movies.id = ratings.movie_id
+WHERE ratings.rating >= 9.0
